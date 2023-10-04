c2532e6491a31e9007fc499e9bc795b869295d6f
TOMMY MANOKOUNE <tommy.manokoune@gmail.com>
2023-10-04 00:48:12 -0700

    done

---
+++ script.js
@@ -0,0 +1,9 @@
+
+let flavors = prompt("Please enter a list of comma-separated froyo flavors ")
+flavors;
+flavorList = {};
+let flavor = flavors.split(",");
+for (var i=0; i<flavor.length; i++){
+    flavorList[flavor[i]] = (flavorList[flavor[i]] ||0) +1;
+}
+console.log(flavorList);
